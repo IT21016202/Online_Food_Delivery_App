206d07a1c72f447829eef902e25ce69a5c30e433
Amisha Prathyanga <it21132582@my.sliit.lk>
2022-10-15 16:32:09 +0530

    second commit

--- app/src/main/java/com/example/foodapp/DBHandler.java
+++ app/src/main/java/com/example/foodapp/DBHandler.java
@@ -9,6 +9,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHandler extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "Instania";
+    public static final String TABLE_NAME_1 = "enrol";
+
+    //Enrol table column names
+    private static final String ID_1 = "id";
+    private static final String PHONE_1 = "phone";
+    private static final String NAME_1 = "name";
+    private static final String ADDRESS_1 = "address";
 
     public DBHandler(Context context) {
         super(context, DB_NAME, null, 1);
@@ -27,11 +34,26 @@ public class DBHandler extends SQLiteOpenHelper {
                         UserTable.Users.COLUMN_REG_DATE + " )";
 
         sqLiteDatabase.execSQL(DbEntries);
+
+        String TABLE_CREATE_QRY = "CREATE TABLE " +TABLE_NAME_1+ " " +
+                "("
+                +ID_1+"INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +PHONE_1 + " INTEGER,"
+                +NAME_1 + " TEXT,"
+                +ADDRESS_1 + " TEXT" +
+                ");";
+
+        sqLiteDatabase.execSQL(TABLE_CREATE_QRY);
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        String DROP_TABLE_QUERY = "DROP TABLE IF EXISTS "+ TABLE_NAME_1;
+        // Drop older table if existed
+        sqLiteDatabase.execSQL(DROP_TABLE_QUERY);
+        // Create tables again
+        onCreate( sqLiteDatabase);
     }
 
     public void registerUser(UserModel userModel){
@@ -50,5 +72,19 @@ public class DBHandler extends SQLiteOpenHelper {
         sqLiteDatabase.close();
     }
 
+    public void addEnrol(Enrol enrol) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(PHONE_1, enrol.getPhone());
+        contentValues.put(NAME_1, enrol.getName());
+        contentValues.put(ADDRESS_1, enrol.getAddress());
+
+        //save to table
+        sqLiteDatabase.insert(TABLE_NAME_1,null,contentValues);
+        // close database
+        sqLiteDatabase.close();
+    }
 }
 
--- app/src/main/java/com/example/foodapp/enrolment.java
+++ app/src/main/java/com/example/foodapp/enrolment.java
@@ -1,9 +1,11 @@
 package com.example.foodapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,19 +13,44 @@ public class enrolment extends AppCompatActivity {
 
     private Button submitBtn;
     private Button canclOdrBtn;
+    private EditText name, phone, address;
+    private Context context;
+    private DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enrolment);
 
-        submitBtn = (Button) findViewById(R.id.btnSubmit1);
+        name = findViewById(R.id.name_txt);
+        phone = findViewById(R.id.phone_txt);
+        address = findViewById(R.id.address_txt);
+        submitBtn = findViewById(R.id.btnSubmit1);
+        context = this;
+
+        dbHandler = new DBHandler(context);
+
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openOdrSummry();
+                String Userphone = phone.getText().toString();
+                String Username = name.getText().toString();
+                String Useraddress = address.getText().toString();
+
+                Enrol enrol = new Enrol(Integer.parseInt(Userphone), Username, Useraddress);
+                dbHandler.addEnrol(enrol);
+
+                startActivity(new Intent(context,order_summary.class ));
             }
         });
+
+//        submitBtn = (Button) findViewById(R.id.btnSubmit1);
+//        submitBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                openOdrSummry();
+//            }
+//        });
 
         canclOdrBtn = (Button) findViewById(R.id.btncanclOdr1);
         canclOdrBtn.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +66,8 @@ public class enrolment extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void openOdrSummry() {
-        Intent intent = new Intent(this, order_summary.class);
-        startActivity(intent);
-    }
+//    private void openOdrSummry() {
+//        Intent intent = new Intent(this, order_summary.class);
+//        startActivity(intent);
+//    }
 }
