903b9cd71c3eb2f4b6b6262be08f302f273d0c24
Pasindu Shyaminda <it21016202@my.sliit.lk>
2022-10-20 18:19:14 +0530

    resolving_conflicts

--- app/src/main/java/com/example/foodapp/DBHandler.java
+++ app/src/main/java/com/example/foodapp/DBHandler.java
@@ -24,9 +24,9 @@ public class DBHandler extends SQLiteOpenHelper {
                         UserTable.Users.COLUMN_FULL_NAME + " TEXT," +
                         UserTable.Users.COLUMN_EMAIL + " TEXT,"  +
                         UserTable.Users.COLUMN_BIRTHDATE + " DATE," +
-                        UserTable.Users.COLUMN_MOBILE + " TEXT, " +
-                        UserTable.Users.COLUMN_PASSWORD + " TEXT, " +
-                        UserTable.Users.COLUMN_REG_DATE + " DATE )";
+                        UserTable.Users.COLUMN_MOBILE + " TEXT," +
+                        UserTable.Users.COLUMN_PASSWORD + " TEXT," +
+                        UserTable.Users.COLUMN_REG_DATE + " DATE)";
 
         sqLiteDatabase.execSQL(DbEntries);
     }
